9c4f6f43a7828e7e41d6773b28404c00e5f918a7
Emre Kurt <emrekurtmf@gmail.com>
2023-01-31 16:05:00 +0300

    edited footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,9 +5,21 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full flex justify-center align-center p-5">
-      <Link href="https://github.com/ekurt/rick-and-morty-api" target="_blank">
-        Github
-      </Link>
+      <p className="text-center">
+        To contribute, visit my{" "}
+        <Link
+          href="https://github.com/ekurt/rick-and-morty-api"
+          target="_blank"
+          className="underline"
+        >
+          github repo
+        </Link>
+        . Visit{" "}
+        <Link href="https://ekurt.dev" target="_blank" className="underline">
+          my website
+        </Link>{" "}
+        for my other projects.
+      </p>
     </footer>
   );
 }
